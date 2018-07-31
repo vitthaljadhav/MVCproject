fb6e60fc07023c6878d4cdfe4b52aaa925044396
INTEL <INTEL@INTEL-PC>
2018-07-31 12:21:24 +0530

    add m1() method in TestCheck class

--- TestGibHub/src/TestCheck.java
+++ TestGibHub/src/TestCheck.java
@@ -1,6 +1,10 @@
 
 public class TestCheck {
 
+	public void m1()
+	{
+		System.out.println("m1()");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
